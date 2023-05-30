f3f2d6971eebb5c253933a7f29b21a6b246f8205
Kaya-F <kfisher@appdev.yearup.org>
2023-05-30 11:52:45 -0400

    recieptFileManager changes.

--- src/main/java/com/jt/ReceiptFileManager.java
+++ src/main/java/com/jt/ReceiptFileManager.java
@@ -1,6 +1,9 @@
 package com.jt;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ReceiptFileManager {
     public void writeReceiptToFile(String receiptData, String fileName) throws IOException {
         FileWriter writer = null;
@@ -12,5 +15,12 @@ public class ReceiptFileManager {
                 writer.close();
             }
         }
+    }
+
+    public String addDateTimeToReceipt(String receiptData) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        return formattedDateTime + "    " + receiptData;
     }
 }
