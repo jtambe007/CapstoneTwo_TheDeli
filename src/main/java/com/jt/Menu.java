90296a8a02c5c1351aa7abaa3af4a31c53e34bea
seolah97 <sbyun@appdev.yearup.org>
2023-05-29 23:53:17 -0400

    Full Add Sandwich Method, with minor issues

--- src/main/java/com/jt/Menu.java
+++ src/main/java/com/jt/Menu.java
@@ -3,38 +3,40 @@ package com.jt;
 import java.util.Scanner;
 
 public class Menu {
-    public static void addSandwich() {
+    public static void AddSandwich() {
         Scanner scanner = new Scanner(System.in);
 
         // Prompt user for Sandwich Size
-        System.out.println("What Size Sandwich would you like?" +
-                "\nWe have 3 Sizes: 4in., 8in., and 12in. ");
-        String size = scanner.nextLine();
+        String addExtraSandwich;
+        do {
+            System.out.println("What Size Sandwich would you like?" +
+                    "\nWe have 3 Sizes: 4in., 8in., and 12in. ");
+            String size = scanner.nextLine();
 
-        // Prompt user for Bread Type
-        System.out.println("What type of bread would you like?" +
-                "\nWe have white, wheat, rye, or a wrap. ");
-        String bread = scanner.nextLine();
+            // Prompt Customer for Bread Type
+            System.out.println("What type of bread would you like?" +
+                    "\nWe have white, wheat, rye, or a wrap. ");
+            String bread = scanner.nextLine();
 
-        // Prompt user for Toppings (Meat)
+            // Prompt Customer for Meat Topping
 
             System.out.println("Would you like to add any Meat?");
-            String addMeat = scanner.nextLine();
+            String answer = scanner.nextLine();
 
-            if (addMeat.equalsIgnoreCase("yes")) {
+            if (answer.equalsIgnoreCase("yes")) {
                 String extraMeat = null;
                 do {
                     System.out.println("What meat would you like to add?" +
                             "\nWe have: Steak, Ham, Salami, Roast Beef, Chicken, and Bacon.");
-                    scanner.nextLine();
+                    String addMeat = scanner.nextLine();
 
-                    // Prompt user if they wish to add extra meat
+                    // Prompt Customer to Add Extra Meat
                     System.out.println("Would you like to add extra meat?" +
                             "\n There will be an up charge.");
-                    extraMeat= scanner.nextLine();
-                } while (!extraMeat.equalsIgnoreCase("yes")); // Will loop back to meat question if "yes"
-
-            } else { // Prompt user for Toppings (Cheese)
+                    extraMeat = scanner.nextLine();
+                } while (extraMeat.equalsIgnoreCase("yes")); // Will loop back to meat question if "yes"
+                    // FIX : extraMeat = no  It needs to ask for cheese next, not toppings.
+            } else { // Prompt Customer for Toppings (Cheese)
                 System.out.println("Would you like to add Cheese?");
                 String addCheese = scanner.nextLine();
                 if (addCheese.equalsIgnoreCase("yes")) {
@@ -44,18 +46,43 @@ public class Menu {
                                 "\nWe have: American, Provolone, Cheddar, and Swiss. ");
                         String cheese = scanner.nextLine();
 
-                        // Prompt user if they wish to add extra cheese
+                        // Prompt Customer to Add Extra Cheese
                         System.out.println("Would you like to add extra cheese?" +
                                 "\n There will be an up charge.");
                         extraCheese = scanner.nextLine();
-                    } while (!extraCheese.equalsIgnoreCase("yes"));
+                    } while (extraCheese.equalsIgnoreCase("yes"));
 
+                    //Fixed extras loop, but need them to loop to the next topping if no extras.
                 } else {
                     System.out.println("What toppings would you like to add?");
                 }
             }
+            // Prompt Customer for Regular Toppings
+            System.out.println("What toppings would you like to add? We have: ");
+            System.out.println("Lettuce, Peppers, Onions, Tomatoes, Jalapenos" +
+                    "\nCucumbers, Pickles, Guacamole, and Mushrooms.");
+            String toppings = scanner.nextLine();
 
+            System.out.println("Would you like to add any Sauces?");
+            String addSauce = scanner.nextLine();
+            if (addSauce.equalsIgnoreCase("yes")) {
+                System.out.println("What Sauces would you like to add? We have:" +
+                        "\n Mayo, Mustard, Ketchup, Ranch, Au Jus," +
+                        "\n Thousand Island, and Vinaigrette.");
+                String sauce = scanner.nextLine();
+            } else {
+                System.out.println("Would you like your sandwich toasted?");
+                String toasted = scanner.nextLine();
+            }
+            System.out.println("Would you like your sandwich toasted?"); // figure out how to dry
+            String toasted = scanner.nextLine(); // fix later
+            System.out.println("You have finished building a Sandwich.");
 
+            // Gives Customer Option to Build Another Sandwich;
+            System.out.println("Would you like to add another Sandwich?");
+            addExtraSandwich = scanner.nextLine();
+            if (addExtraSandwich.equalsIgnoreCase("no"));
+        } while (addExtraSandwich.equalsIgnoreCase("yes"));
     }
 
 }
