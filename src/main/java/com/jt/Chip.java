605302048f05701488bee94c2506137a8ea3a080
Jackie <marietambe07@gmail.com>
2023-05-30 14:13:33 -0400

    updated child classes

--- src/main/java/com/jt/Chip.java
+++ src/main/java/com/jt/Chip.java
@@ -2,8 +2,10 @@ package com.jt;
 
 public class Chip extends Product{
     private String flavor;
-    public Chip(String name, float price, int quantity, String flavor) {
-        super(name, price, quantity);
+    private String name;
+    public Chip(float price, int quantity, String flavor) {
+        super(price, quantity);
+        this.name = name;
         this.flavor = flavor;
     }
 }
--- src/main/java/com/jt/Sandwich.java
+++ src/main/java/com/jt/Sandwich.java
@@ -2,7 +2,7 @@ package com.jt;
 
 public class Sandwich extends Product{
 
-    public Sandwich(String name, float price, int quantity, String size) {
-        super(name, price, quantity, size);
+    public Sandwich(float price, int quantity, String size) {
+        super(price, quantity, size);
     }
 }
