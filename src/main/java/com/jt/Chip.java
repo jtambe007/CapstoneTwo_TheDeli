f51af8cb980f7a318ddc2d3052fac361ef7ab8b2
seolah97 <sbyun@appdev.yearup.org>
2023-06-01 13:05:34 -0400

    updated Chip Class

---
+++ src/main/java/com/jt/Chip.java
@@ -0,0 +1,25 @@
+package com.jt;
+
+    public class Chip extends Product {
+        private String name;
+
+        public Chip(int quantity, String name) {
+            super(1.50f, quantity);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Chip Qty:" + getQuantity() + " " +
+                    getName() + " $" + getPrice();
+        }
+    }
+}
